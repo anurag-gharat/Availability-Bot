33608f5f1960e438350314806e208b3281a4f3f4
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-03-31 16:45:47 +0530

    "Link data"

--- titlesentiment.py
+++ titlesentiment.py
@@ -1,3 +1,16 @@
 from textblob import TextBlob
 import requests
 from bs4 import BeautifulSoup
+
+class Analysis:
+    def  __init__(self,term):
+        self.term=term
+        self.subjectivity=0
+        self.sentiment=0
+        self.url='https://www.google.com/search?q=[0]&source='.format(self.term)
+
+
+    def run(self):
+        resonse=requests.get(self.url)
+        soup=BeautifulSoup(resonse.text,'html.parser')
+        headline_results=soup.find_all('div',class.='st')
