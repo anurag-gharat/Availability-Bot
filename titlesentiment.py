8836831c01a4797c7687b8754e9800bfab4823bc
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-03-31 17:03:25 +0530

    'Check the network issues'

--- titlesentiment.py
+++ titlesentiment.py
@@ -7,10 +7,16 @@ class Analysis:
         self.term=term
         self.subjectivity=0
         self.sentiment=0
-        self.url='https://www.google.com/search?q=[0]&source='.format(self.term)
-
-
+        self.url = 'https://www.google.com/search?q={0}&source=lnms&tbm=nws'.format(self.term)
     def run(self):
-        resonse=requests.get(self.url)
-        soup=BeautifulSoup(resonse.text,'html.parser')
-        headline_results=soup.find_all('div',class.='st')
+		    response = requests.get(self.url)
+		    soup = BeautifulSoup(response.text, 'html.parser')
+		    headline_results = soup.find_all('div', class_='st')
+		    for text in headline_results:
+			                        blob = TextBlob(text.get_text())	
+			                        self.sentiment += blob.sentiment.polarity / len(headline_results)
+			                        self.subjectivity += blob.sentiment.subjectivity / len(headline_results)
+
+a = Analysis('corona')
+a.run()
+print(a.term.a.subjectivity,a.sentiment)
